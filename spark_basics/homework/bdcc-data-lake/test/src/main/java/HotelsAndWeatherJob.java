533d30ae880b5b520efe5c7635bcb01b17e77e08
Eldar Kalachev <rusgunter@gmail.com>
2021-04-03 14:52:21 +0300

    connection to azure through hadoop

--- spark_basics/homework/bdcc-data-lake/test/src/main/java/HotelsAndWeatherJob.java
+++ spark_basics/homework/bdcc-data-lake/test/src/main/java/HotelsAndWeatherJob.java
@@ -7,68 +7,39 @@ import com.azure.storage.blob.models.StorageAccountInfo;
 import com.azure.storage.file.datalake.DataLakeServiceClient;
 import com.azure.storage.file.datalake.DataLakeServiceClientBuilder;
 import com.azure.storage.file.datalake.models.FileSystemItem;
+import org.apache.spark.SparkConf;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Encoders;
+import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
-import scala.collection.immutable.Seq;
-import scala.reflect.io.ZipArchive;
 import utilities.Hotel;
 
 import java.io.*;
 import java.time.Duration;
-import java.util.zip.ZipOutputStream;
 
 public class HotelsAndWeatherJob {
-    public static void main(String[] args) {
-        readFromAzure();
-    }
+    private static final String containerName = "m6sparkbasics";
+    private static final String accountName = "bd201stacc";
+    private static final String endpoint = "https://bd201stacc.blob.core.windows.net";
+    private static final String sasToken = "sv=2020-04-08&st=2021-03-30T12%3A46%3A05Z&se=2031-03-31T12%3A46%3A00Z&sr=c&sp=rl&sig=H7H5Q2Tq5KL21wXPvIaI2mnVZYdoUEotK1Y%2Bca0NGNE%3D";
 
-    private static void readFromAzure() {
-        String containerName = "<container-name>";
 
-        BlobServiceClient blobServiceClient = new BlobServiceClientBuilder()
-                .endpoint("<end-point>")
-                .sasToken("<sas-token>")
-                .buildClient();
-
-        BlobContainerClient blobContainerClient = blobServiceClient
-                .getBlobContainerClient(containerName);
-
-        ListBlobsOptions options = new ListBlobsOptions()
-                .setPrefix("hotels/part")
-                .setDetails(new BlobListDetails()
-                        .setRetrieveDeletedBlobs(false)
-                        .setRetrieveSnapshots(false));
-
-        blobContainerClient
-                .listBlobs(options, Duration.ofMinutes(10))
-                .forEach(blob -> processHotel(blobContainerClient, blob));
-    }
-
-    public static void processHotel(BlobContainerClient blobContainerClient, BlobItem blob) {
-        System.out.printf("Name: %s%n", blob.getName());
-
-        BlobClient blobClient = blobContainerClient.getBlobClient(blob.getName());
-
-        ByteArrayOutputStream  outputStream = new ByteArrayOutputStream();
-
-        blobClient.download(outputStream);
-
-        final byte[] bytes = outputStream.toByteArray();
-        ByteArrayInputStream inputStream = new ByteArrayInputStream(bytes);
-
-        SparkSession spark = SparkSession.builder()
+    public static void main(String[] args) throws InterruptedException {
+        SparkSession spark = SparkSession
+                .builder()
                 .master("local[*]")
                 .appName("HotelsAndWeatherJob")
                 .getOrCreate();
 
-//        Seq<Hotel> hotels = inputStream
-//                .getClass()
-//                .getClassLoader()
-//                .getResourceAsStream("hotels.csv.zip");
+//       Thread.sleep(10 * 60 * 1000);
 
+        spark
+                .read()
+                .format("csv")
+                .option("header", "true")
+                .load("abfss://" + containerName + "@" + accountName + ".dfs.core.windows.net/hotels")
+                .show();
 
-
-//        Dataset dataset = spark.createDataset(inputStream.read(), Encoders.bean(Hotel.class));
+//        Dataset dataset = spark.createDataset(, Encoders.bean(Hotel.class))
     }
 }
