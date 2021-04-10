9a20906fe5d6bdd751ca7f0ca114cfdf333db875
Eldar Kalachev <rusgunter@gmail.com>
2021-04-10 03:43:38 +0300

    tried to deploy the spark application on local pod

--- spark_basics/homework/bdcc-data-lake/hotel-weather-job/entrypoint.sh
+++ spark_basics/homework/bdcc-data-lake/hotel-weather-job/entrypoint.sh
@@ -5,10 +5,22 @@ readonly EXECUTOR_MEMORY='3g'
 readonly KUBERNETES_CONTAINER_IMAGE="spark/hotel-weather-job"
 readonly SOURCE_DIR="/tmp/spark-k8s-demo"
 
-# Execute the container CMD under tini for better hygiene
-exec /sbin/tini -s -- spark-submit --master "${SPARK_MASTER}" \
---deploy-mode cluster \
---name hotels-weather \
+readonly SPARK_NAMESPACE=default
+readonly SA=spark-minion
+readonly K8S_CACERT=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
+readonly K8S_TOKEN=/var/run/secrets/kubernetes.io/serviceaccount/token
+
+readonly PROJECT='hotel-weather-job'
+
+#oc create sa spark -n ${PROJECT}
+#oc adm policy add-scc-to-user anyuid -z spark -n ${PROJECT}
+
+exec /opt/spark/bin/spark-submit \
+--verbose \
+--master "${SPARK_MASTER}" \
+--name ${PROJECT} \
+--conf spark.submit.deployMode=cluster \
+--conf spark.kubernetes.namespace=${PROJECT} \
 --conf spark.jars.ivy=/tmp/.ivy \
 --conf spark.kubernetes.container.image="${KUBERNETES_CONTAINER_IMAGE}" \
 --conf spark.hadoop.fs.azure.account.key."${BLOB_READ_ACCOUNT_NAME}".dfs.core.windows.net="${BLOB_READ_CONTAINER_KEY}" \
@@ -17,10 +29,10 @@ exec /sbin/tini -s -- spark-submit --master "${SPARK_MASTER}" \
 --conf spark.driver.memory=${DRIVER_MEMORY} \
 --conf spark.executor.memory=${EXECUTOR_MEMORY} \
 --conf spark.executor.instances=2 \
---conf spark.hadoop.security.authentication=simple \
---conf spark.hadoop.security.authorization=false \
---conf spark.kubernetes.authenticate.caCertFile=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt  \
---conf spark.kubernetes.authenticate.oauthTokenFile=/var/run/secrets/kubernetes.io/serviceaccount/token  \
---conf spark.kubernetes.authenticate.driver.serviceAccountName=spark \
+--conf spark.kubernetes.authenticate.subdmission.caCertFile=$K8S_CACERT  \
+--conf spark.kubernetes.authenticate.submission.oauthTokenFile=$K8S_TOKEN  \
+--conf spark.kubernetes.authenticate.driver.serviceAccountName=$SA  \
+--conf spark.kubernetes.container.image.pullPolicy=Always \
+--conf spark.kubernetes.namespace=$SPARK_NAMESPACE  \
 --class "HotelWeatherJob" \
 "/opt/spark/jars/hotel-weather-job.jar"
